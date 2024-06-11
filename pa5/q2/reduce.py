aabe9f8bc92b04ad3947de2134b9aa8e2e89e401
Marc Owen <marcowen20@gmail.com>
2024-06-11 19:28:43 +0900

    Restructure folder

--- pa5/q2/reduce.py
+++ pa5/q2/reduce.py
@@ -1,43 +1,35 @@
 #!/usr/bin/python3
 import sys
-import statistics as s
 
-# student(sid, firstname, lastname, year, deptno, gpa) 
-# dept(deptno, dname, campus, building) as follows. 
+players_map = {} # name => (goals, assists)
+dominated_map = {} # name => [name2 if name2 dominates name]
 
-#TODO: Consider when student has dept not in dept
-
-gpa_map = {}
-name_campus_map = {}
-
+# Store player information
 for line in sys.stdin:
-    line = line.strip()
-    attrs = line.split(',')
-    if (attrs[1] == "student"):
-        deptno, _, gpa = attrs
+    line = line.strip().split(',')
+    name, goals, assists = line
+    players_map[name] = (int(goals), int(assists))
 
-        if deptno not in gpa_map:
-            gpa_map[deptno] = []
-        gpa_map[deptno].append(gpa)
+# Compute dominated_map
+for name, (goals, assists) in players_map.items():
+    for name2, (goals2, assists2) in players_map.items():
+        if name == name2:
+            continue
 
-    elif (attrs[1] == "dept"):
-        deptno, _, dname, campus = attrs
+        if name2 not in dominated_map:
+            dominated_map[name2] = []
 
-        name_campus_map[deptno] = (dname, campus)
-    else:
-        exit(-1)
+        # Condition 1
+        # attrs of name >= attrs of name2 
+        if goals >= goals2 and assists >= assists2:
+            # Condition 2
+            # At least one attr of name > attr of name2
+            if goals > goals2 or assists > assists2:
+                dominated_map[name2].append(name)
 
-for deptno in gpa_map.keys():
-    # Skip if student's department doesn't exist
-    if deptno not in name_campus_map.keys():
-        continue
-
-    gpas = [float(gpa) for gpa in gpa_map[deptno]]
-    avg_gpa = s.mean(gpas)
-    # Skip if average gpa <= 2.5
-    if avg_gpa <= 2.5:
-        continue
-
-    max_gpa = max(gpas)
-    dname, campus = name_campus_map[deptno]
-    print(f"{dname}, {max_gpa}, {campus}")
+# Nominees are players that are not dominated by any other player
+nominees = []
+for name, dominators in dominated_map.items():
+    if len(dominators) == 0:
+        nominees.append(name)
+print("\n".join(sorted(nominees)))
